04959287663da65f9c68152a45b792468ea6da79
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-08-29 22:23:57 -0500

    data now displays on html file

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var inquirer = require('inquirer');
-const { waitForDebugger } = require('inspector');
-const { functions } = require('lodash');
-var eif = 'manager';
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const fs = require('fs');
+let employeeArr = [];
+let someArr = [];
 
 const managerQuestions = [
     {
@@ -100,7 +103,7 @@ function askForManagerInfo() {
     return inquirer
         .prompt(managerQuestions)
         .then((answers) => {
-            addToHTML(answers);
+            employeeArr.push(new Manager(answers.name, answers.id, answers.email, answers.officeNum));
             decide(answers)
         })
         .catch((error) => {
@@ -118,7 +121,7 @@ function decide(answers) {
     } else if (answers.eif == 'Add Intern') {
         askForInternInfo();
     } else {
-        return; //turn into creating the html file
+        createHTML();
     }
 }
 
@@ -126,7 +129,7 @@ function askForEngineerInfo() {
     return inquirer
         .prompt(engineerQuestions)
         .then((answers) => {
-            addToHTML(answers);
+            employeeArr.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
             decide(answers)
         })
         .catch((error) => {
@@ -142,7 +145,7 @@ function askForInternInfo() {
     return inquirer
         .prompt(internQuestions)
         .then((answers) => {
-            addToHTML(answers);
+            employeeArr.push(new Intern(answers.name, answers.id, answers.email, answers.school));
             decide(answers)
         })
         .catch((error) => {
@@ -154,6 +157,78 @@ function askForInternInfo() {
         })
 }
 
-function addToHTML(answers) {
+function createHTML() {
+
+    let employeeList = '';
+    for(let i = 0; i < employeeArr.length; i++){
+        if(employeeArr[i].officeNum){
+            employeeList += 
+                `<li>
+                    <div class="top-box">
+                        <h2>${employeeArr[i].name}</h2>
+                        <h3>Manager</h3>
+                    </div>
+
+                    <div class="bottom-box">
+                        <h4>ID: ${employeeArr[i].id}</h4>
+                        <h4>Email: ${employeeArr[i].email}</h4>
+                        <h4>Office Number: ${employeeArr[i].officeNum}</h4>
+                    </div>
+                </li>`
+        } else if (employeeArr[i].github) {
+            employeeList += 
+                `<li>
+                    <div class="top-box">
+                        <h2>${employeeArr[i].name}</h2>
+                        <h3>Engineer</h3>
+                    </div>
+
+                    <div class="bottom-box">
+                        <h4>ID: ${employeeArr[i].id}</h4>
+                        <h4>Email: ${employeeArr[i].email}</h4>
+                        <h4>GitHub Username: ${employeeArr[i].github}</h4>
+                    </div>
+                </li>`
+        } else {
+            employeeList += 
+                `<li>
+                    <div class="top-box">
+                        <h2>${employeeArr[i].name}</h2>
+                        <h3>Intern</h3>
+                    </div>
+
+                    <div class="bottom-box">
+                        <h4>ID: ${employeeArr[i].id}</h4>
+                        <h4>Email: ${employeeArr[i].email}</h4>
+                        <h4>Office Number: ${employeeArr[i].school}</h4>
+                    </div>
+                </li>`
+        }
+    }
     
-}
+    fs.writeFileSync('./dist/rendered.html',`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Employee Info Generator</title>
+            <link rel="stylesheet" href="./style.css">
+            <script src="jquery-3.6.0.min.js"></script>
+        </head>
+        <body>
+            <header>
+                <h1>Team Members</h1>
+            </header>
+        
+            <div id="employee-container">
+                <ul>
+                    ${employeeList}
+                </ul>
+            </div>
+        
+            <script src="../index.js"></script>
+            
+        </body>
+        </html>`);
+    }
